7cd69141869678a51358eefef21fef3156a7f923
ivogz <ivogz.01@gmail.com>
2023-01-30 12:43:57 -0300

    Prueba1

---
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -0,0 +1,10 @@
+/*
+Debemos lograr tomar un nombre con 'prompt' 
+y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
+function mostrar()
+{
+	let nombre;
+	nombre = prompt("Cuál es tu nombre");
+	alert(nombre)
+}
+
